453e4cd4a268f8c8c2aab4d3bc5dba79edb66fd3
Kensa <kensadevs@gmail.com>
2021-01-13 22:43:43 +0100

    first commit

---
+++ electron-starter.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path');
+const url = require('url');
+let mainWindow;
+function createWindow() {
+    mainWindow = new BrowserWindow({width: 1000, height: 800});
+    mainWindow.loadURL('http://localhost:3000');
+    //mainWindow.webContents.openDevTools();
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    })
+}
+app.on('ready', createWindow);
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
+
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow()
+    }
+});
