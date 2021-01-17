15c56390344037202b6f207733d95590eacb0744
Kensa <kensadevs@gmail.com>
2021-01-17 01:59:15 +0100

    DORMIR !!!!!!!!

--- src/widget.js
+++ src/widget.js
@@ -1,15 +1,11 @@
 import React from 'react';
 
 export default class Widget extends React.Component{
-   constructor(props){
-        super(props);
-    }
-    
     render(){
         return(
             <div className="widget">
                 <div className="widget_img">
-                    <img src={"http://localhost:3000/img/"+this.props.src+".png"}></img>
+                    <img src={"http://localhost:3000/img/"+this.props.src+".png"} alt="joli img"></img>
                 </div>
                 <div className="widget_textbox">
                     <span className="widget_text">{this.props.text}</span>
